353293435fd5549739d2a0f3d719a8fbec9fd735
Kenichi Kamiya <kachick1@gmail.com>
2022-10-26 00:22:57 +0900

    `shfmt --write --indent 0 **/*.bash`

--- example/show_4th-monday_in_next_year.bash
+++ example/show_4th-monday_in_next_year.bash
@@ -3,17 +3,17 @@
 LANG=C
 export LANG
 own_path=$(
-  cd "$(dirname "$0")" || exit
-  pwd
+	cd "$(dirname "$0")" || exit
+	pwd
 )
 pj_path=$(dirname "$own_path")
 # shellcheck source=lib/extoday.bash
 . "$pj_path/lib/extoday.bash"
 
 for n in $(seq 0 365); do
-  date=$(date --date "$n day" '+%Y-%m-%d')
+	date=$(date --date "$n day" '+%Y-%m-%d')
 
-  if [ "$(show_nth_week_in_month "$date")" = 4 ] && [ "$(show_wday "$date")" = Mon ]; then
-    echo "$date"
-  fi
+	if [ "$(show_nth_week_in_month "$date")" = 4 ] && [ "$(show_wday "$date")" = Mon ]; then
+		echo "$date"
+	fi
 done
